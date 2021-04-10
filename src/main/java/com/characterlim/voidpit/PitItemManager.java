fbe99a782b9c4f77e277bfe185fbe8498f0f5e02
Ben Myers <benl.myers01@gmail.com>
2021-04-10 12:59:16 -0700

    📰 Config manager hooks into item/region managers

--- src/main/java/com/characterlim/voidpit/PitItemManager.java
+++ src/main/java/com/characterlim/voidpit/PitItemManager.java
@@ -9,26 +9,21 @@ import java.util.List;
 public class PitItemManager {
 
     private final VoidPitPlugin plugin;
-    private final List<Material> items = new ArrayList<>();
 
     public PitItemManager(VoidPitPlugin instance) {
         this.plugin = instance;
-        List<String> itemStrings = (List<String>) plugin.getConfig().getList("accepted-items");
-        if(itemStrings != null) {
-            for(String itemString: itemStrings) items.add(Material.getMaterial(itemString));
-        }
     }
 
     public void setItem(Player player) {
-        items.clear();
-        items.add(getItem(player));
+        Config.Item.items.clear();
+        Config.Item.items.add(getItem(player));
         player.sendMessage("§bVoid Pit accepted item set to §9" + getItem(player).toString().toLowerCase());
         updateConfig();
     }
 
     public void addItem(Player player) {
-        items.add(getItem(player));
-        if(items.contains(getItem(player).toString().toLowerCase())) {
+        Config.Item.items.add(getItem(player));
+        if(Config.Item.items.contains(getItem(player).toString().toLowerCase())) {
             player.sendMessage("§9" + getItem(player).toString().toLowerCase() + " §bis already a part of the accepted items list!");
             return;
         }
@@ -37,23 +32,23 @@ public class PitItemManager {
     }
 
     public void removeItem(Player player) {
-        items.remove(getItem(player));
+        Config.Item.items.remove(getItem(player));
         player.sendMessage("§9" + getItem(player).toString().toLowerCase() + " §bremoved from Void Pit accepted items list.");
         updateConfig();
     }
 
     public void resetItems(Player player) {
-        items.clear();
+        Config.Item.items.clear();
         player.sendMessage("§bVoid Pit accepted items have been reset.");
         updateConfig();
     }
 
     public void listItems(Player player) {
-        if(items.size() == 0) {
+        if(Config.Item.items.size() == 0) {
             player.sendMessage("§bYour pit does not accept any items. Use §e/pit item add §bto add new items.");
         }
-        StringBuilder message = new StringBuilder("§bVoid Pit accepted items (" + items.size() + "): ");
-        for(Material material : items) {
+        StringBuilder message = new StringBuilder("§bVoid Pit accepted items (" + Config.Item.items.size() + "): ");
+        for(Material material : Config.Item.items) {
             message.append("§9").append(material.toString().toLowerCase()).append("§b, ");
         }
         message.setLength(message.length() - 2);
@@ -61,7 +56,7 @@ public class PitItemManager {
     }
 
     public List<Material> getItems() {
-        return this.items;
+        return Config.Item.items;
     }
 
     private Material getItem(Player player) {
@@ -70,7 +65,7 @@ public class PitItemManager {
 
     private void updateConfig() {
         List<String> itemStrings = new ArrayList<String>();
-        for(Material item : items) itemStrings.add(item.toString());
+        for(Material item : Config.Item.items) itemStrings.add(item.toString());
         plugin.getConfig().addDefault("accepted-items", itemStrings);
         plugin.saveConfig();
     }
--- src/main/java/com/characterlim/voidpit/PitRegionManager.java
+++ src/main/java/com/characterlim/voidpit/PitRegionManager.java
@@ -18,14 +18,10 @@ public class PitRegionManager {
 
     private final VoidPitPlugin plugin;
     private WorldEditPlugin worldEditPlugin = null;
-    private Location pos1 = null;
-    private Location pos2 = null;
 
     public PitRegionManager(VoidPitPlugin instance) {
         this.plugin = instance;
         this.worldEditPlugin = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
-        this.pos1 = instance.getConfig().getLocation("region-corner1");
-        this.pos2 = instance.getConfig().getLocation("region-corner2");
     }
 
     public void setRegion(Player player) {
@@ -35,9 +31,9 @@ public class PitRegionManager {
             Region region = worldEditPlugin.getSession(player).getSelection(world);
             BlockVector3 v1 = region.getMinimumPoint();
             BlockVector3 v2 = region.getMaximumPoint();
-            this.pos1 = new Location(player.getWorld(), (double) v1.getX(), (double) v1.getY(), (double) v1.getZ());
-            this.pos2 = new Location(player.getWorld(), (double) v2.getX(), (double) v2.getY(), (double) v2.getZ());
-            player.sendMessage("§bRegion set! Corner 1: §9" + this.pos1.toString() + "§b | Corner 2: §9" + this.pos2.toString());
+            Config.Region.pos1 = new Location(player.getWorld(), (double) v1.getX(), (double) v1.getY(), (double) v1.getZ());
+            Config.Region.pos2 = new Location(player.getWorld(), (double) v2.getX(), (double) v2.getY(), (double) v2.getZ());
+            player.sendMessage("§bRegion set! Corner 1: §9" + Config.Region.pos1.toString() + "§b | Corner 2: §9" + Config.Region.pos2.toString());
             updateConfig();
         } catch(IncompleteRegionException e) {
             player.sendMessage("§bYou haven't selected a complete region! Ensure a complete selection is selected, then try again.");
@@ -45,14 +41,14 @@ public class PitRegionManager {
     }
 
     public void showRegion(Player player) {
-        if(pos1 != null && pos2 != null) {
-            player.sendMessage("§bCorner 1: §9" + this.pos1.toString() + "§b | Corner 2: §9" + this.pos2.toString());
+        if(Config.Region.pos1 != null && Config.Region.pos2 != null) {
+            player.sendMessage("§bCorner 1: §9" + Config.Region.pos1.toString() + "§b | Corner 2: §9" + Config.Region.pos2.toString());
         }
     }
 
     private void updateConfig() {
-        plugin.getConfig().addDefault("region-corner1", this.pos1);
-        plugin.getConfig().addDefault("region-corner2", this.pos2);
+        plugin.getConfig().addDefault("region-corner1", Config.Region.pos1);
+        plugin.getConfig().addDefault("region-corner2", Config.Region.pos2);
         plugin.saveConfig();
     }
 }

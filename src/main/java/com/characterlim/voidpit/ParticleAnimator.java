5a7d53619f4dcb3086fc5085dc67a2adfaee0bc7
Ben Myers <benl.myers01@gmail.com>
2021-04-10 20:35:34 -0700

    🎢 Improvements to particle ascend animation

--- src/main/java/com/characterlim/voidpit/ParticleAnimator.java
+++ src/main/java/com/characterlim/voidpit/ParticleAnimator.java
@@ -6,7 +6,7 @@ import org.bukkit.World;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import static java.lang.Math.floor;
+import static java.lang.Math.*;
 
 public class ParticleAnimator {
 
@@ -14,7 +14,7 @@ public class ParticleAnimator {
     private final VoidPitPlugin plugin;
     private static final int ITEM_DESTROY_PARTICLE_COUNT = 25;
     private static final int TPS = 20;
-    private static final int ASCEND_ANIMATION_TICKS = TPS * 3;
+    private static final int ASCEND_ANIMATION_TICKS = TPS * 7;
 
     public ParticleAnimator(World world, VoidPitPlugin instance) {
         this.world = world;
@@ -31,21 +31,25 @@ public class ParticleAnimator {
 
         Location endLocation = new Location(Config.Hologram.pos.getWorld(), Config.Hologram.pos.getX(), Config.Hologram.pos.getY(), Config.Hologram.pos.getZ());
 
+        double height = endLocation.getY() - startLocation.getY();
+        double triggerHeight = startLocation.getY() + (height / 2);
+
         Vector dr = endLocation.subtract(startLocation).toVector().multiply(1.0 / ASCEND_ANIMATION_TICKS);
         final int[] tickCount = {0};
-
-        plugin.getLogger().info("start = " + pos);
-        plugin.getLogger().info("dr = " + dr.toString());
 
         BukkitRunnable task = new BukkitRunnable() {
             @Override
             public void run() {
-                world.spawnParticle(Particle.END_ROD, pos, 1, 0, 0, 0, 0);
-                int dt = 1 + (int)floor(tickCount[0] / 10.0);
+                Vector spawnPos = new Vector(pos.getX(), pos.getY(), pos.getZ());
+                double offset = height/2 - Math.abs(triggerHeight - pos.getY());
+                spawnPos.add(new Vector(0.3 * offset * sin(tickCount[0] * 0.05), 0, 0.3 * offset * cos(tickCount[0] * 0.05)));
+                Location spawnLoc = new Location(startLocation.getWorld(), spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
+                world.spawnParticle(Particle.END_ROD, spawnLoc, 1, 0, 0, 0, 0);
+                int dt = 1 + (int)floor(tickCount[0] / 50.0);
                 tickCount[0] += dt;
                 Vector change = new Vector(dr.getX(), dr.getY(), dr.getZ()).multiply(dt);
                 pos.add(change);
-                plugin.getLogger().info("spawning particle at " + pos);
+                plugin.getLogger().info("offset = " + offset);
                 if(tickCount[0] >= ASCEND_ANIMATION_TICKS) this.cancel();
             }
         };
